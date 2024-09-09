2f97033d3fb4a5bf06264f683934fee2255fe7bf
1v4nk0 <1vancherk3s@gmail.com>
2024-09-09 15:42:41 +0100

    new changes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">ivanko pizza co.</Link>
-
+      <SearchOrder />
       <p>Ivan</p>
     </header>
   );
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function Loader() {
+    return (
+        <div className="loader">
+            
+        </div>
+    )
+}
+
+export default Loader
